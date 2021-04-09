7a461f4ab955b4d01b676f059ea469982b9cadc7
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2021-04-09 22:08:13 +0300

    final

--- src/main.cpp
+++ src/main.cpp
@@ -52,9 +52,6 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
     
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
     const int QUESTIONS = 4;
     std::string inputQuestions [QUESTIONS] = {
         "What is the starting x input? ", 
@@ -71,9 +68,6 @@ int main(int argc, const char **argv)
         user_input.push_back(input);
     }
     float start_x = user_input[0], start_y = user_input[1], end_x = user_input[2], end_y = user_input[3];
-    // float start_x = 10, start_y = 10, end_x = 90, end_y = 90;
-    // std::cout << "temp hard-coding values\n";
-
 
     // Build Model.
     RouteModel model{osm_data};
